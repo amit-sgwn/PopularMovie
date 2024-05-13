f51c10e1803aa2df2bf86811a2c0a18007dbb240
Amit Kumar <amitsangwan898@gmail.com>
2024-05-13 16:13:26 +0530

    Initial Commit

---
+++ PopularMovie/PopularMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMovieApp.swift
+//  PopularMovie
+//
+//  Created by Amit Kumar on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PopularMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
